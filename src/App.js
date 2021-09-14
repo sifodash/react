345c452c095b409b317fa2ba1e568f63c1b2b8da
dave <dbrosnan@steamny.com>
2021-09-14 15:24:37 -0400

    pg 29

--- src/App.js
+++ src/App.js
@@ -2,57 +2,68 @@ import logo from './logo.svg';
 import './App.css';
 import { getByTitle } from '@testing-library/dom';
 
+
+const list = [
+  {
+    title:'React',
+    url: 'https://reactjs.org',
+    author:'Jordan Walke',
+    num_comments: 3,
+    points:4,
+    objectID:0,
+
+  },
+
+  {
+    title:'Redux',
+    url:'https://redux.js.org',
+    author:'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
+
+
+
 function App() {
 
-  function getTitle(title) {
-    return title;
-  }
+
   
-  const list = [
-    {
-      title:'React',
-      url: 'https://reactjs.org',
-      author:'Jordan Walke',
-      num_comments: 3,
-      points:4,
-      objectID:0,
 
-    },
-
-    {
-      title:'Redux',
-      url:'https://redux.js.org',
-      author:'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    }
-  ]
   
   return (
     <div className="App">
       <h1>My Stories</h1>
-      <ul>
-      {
-        list.map(function(item){
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' />
+      <List />
+    </div>
+  )
+}
 
-          return (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
 
+
+function List() {
+  return (
+    <ul>
+      {list.map(function(item){
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        )
       })}
 
-      </ul>
-
-    </div>
-  );
+    </ul>
+  )
 }
+
+
 
 export default App;
