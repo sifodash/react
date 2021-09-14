c171fbb1d39a9b3b11d7669ade3d8cec7f1595ab
sifodash <dbrosnan@steamny.com>
2021-09-14 13:39:30 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { getByTitle } from '@testing-library/dom';
 
 function App() {
+
+  function getTitle(title) {
+    return title;
+  }
+  
+  const list = [
+    {
+      title:'React',
+      url: 'https://reactjs.org',
+      author:'Jordan Walke',
+      num_comments: 3,
+      points:4,
+      objectID:0,
+
+    },
+
+    {
+      title:'Redux',
+      url:'https://redux.js.org',
+      author:'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ]
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Hello {getTitle('React')}</h1>
+      <label htmlFor='search'>Search:</label>
+      <input id='search' type='text' />
     </div>
   );
 }
