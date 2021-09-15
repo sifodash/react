9f4fdc62cd6af411b56b6acd90489b573ad8c9ae
dave <dbrosnan@steamny.com>
2021-09-15 11:46:29 -0400

    pg 45

--- src/App.js
+++ src/App.js
@@ -49,15 +49,10 @@ function List(props) {
   return (
     <ul>
       {props.list.map(function(item){
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
+        return(
+
+        <Item key={item.objectID} item={item} />
+        
         )
       })}
 
@@ -81,5 +76,22 @@ function Search(){
     </div>
   )
 }
+
+
+function Item(props){
+
+  return (
+
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+  )
+}
+
 
 export default App;
