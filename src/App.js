faa34a76b27e627525577b4d54d3dfc383acb713
dave <dbrosnan@steamny.com>
2021-09-15 08:36:09 -0400

    pg 44

--- src/App.js
+++ src/App.js
@@ -3,52 +3,52 @@ import './App.css';
 import { getByTitle } from '@testing-library/dom';
 
 
-const list = [
-  {
-    title:'React',
-    url: 'https://reactjs.org',
-    author:'Jordan Walke',
-    num_comments: 3,
-    points:4,
-    objectID:0,
 
-  },
-
-  {
-    title:'Redux',
-    url:'https://redux.js.org',
-    author:'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-]
 
 
 
 function App() {
 
-
+  const stories = [
+    {
+      title:'React',
+      url: 'https://reactjs.org',
+      author:'Jordan Walke',
+      num_comments: 3,
+      points:4,
+      objectID:0,
+  
+    },
+  
+    {
+      title:'Redux',
+      url:'https://redux.js.org',
+      author:'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ]
   
 
   
   return (
     <div className="App">
       <h1>My Stories</h1>
+      <Search />
      {/* Creating first instance of list */}
-      <List />
-      {/* Creating second instance of list */}
-      <List />
+      <List list={stories}/>
+
     </div>
   )
 }
 
 
 
-function List() {
+function List(props) {
   return (
     <ul>
-      {list.map(function(item){
+      {props.list.map(function(item){
         return (
           <li key={item.objectID}>
             <span>
@@ -67,10 +67,16 @@ function List() {
 
 
 function Search(){
+
+  function handleChange(event){
+    console.log(event.target.value)
+  
+  }
+
   return (
     <div>
       <label htmlFor='search'>Search: </label>
-      <input id='search' type='text' />
+      <input id='search' type='text' onChange={handleChange} />
 
     </div>
   )
